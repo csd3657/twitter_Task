456eb9c5cdcf9be18e6a4dd59fe402cf4a0f8381
dimilat <csd3657@csd.uoc.gr>
2020-08-28 14:18:43 +0300

    search bar functionality and some results

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,12 +8,32 @@ import { DataService } from './data.service';
 })
 export class AppComponent implements OnInit {
   title = 'twitter-app';
-
+  searching_item: String = "";
+  followers_number = 0;
+  friends_number = 0;
   constructor(private DataService: DataService) { }
 
   ngOnInit() {
     this.DataService.fetchData();
-
   }
 
+  Search(){
+    this.DataService.search_token = this.searching_item;
+    this.DataService.fetchData();
+    this.getFollowersCount();
+  }
+
+  getFollowersCount(){
+    this.DataService.followers_number = 0;
+    this.DataService.friends_number = 0;
+
+    for (let i = 0; i < this.DataService._data['mentions'].length; i++) {
+      this.DataService.followers_number += this.DataService._data['mentions'][i].followers_count;
+      this.DataService.friends_number += this.DataService._data['mentions'][i].friends_count;
+      console.log(i, this.DataService._data['mentions'][i].followers_count);
+    }
+
+    this.followers_number = this.DataService.followers_number;
+    this.friends_number = this.DataService.friends_number;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +13,8 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
